33fe48e06a4fcd6aa25b8f8d58fb1712999f15fa
hirakida <hirakida29@gmail.com>
2018-12-01 23:35:17 +0900

    Update demo

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.3.10"
+    val springBootVersion = "2.1.1.RELEASE"
+    id("org.springframework.boot") version springBootVersion
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        javaParameters = true
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:bootstrap:4.1.3") {
+        exclude(module = "jquery")
+    }
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
