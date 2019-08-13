1942025afa3708e45892f1bc828213baa80c21e9
hirakida <hirakida29@gmail.com>
2019-08-13 17:22:38 +0900

    Update

--- kotlin-maven/src/main/kotlin/com/example/ApiController.kt
+++ kotlin-maven/src/main/kotlin/com/example/ApiController.kt
@@ -1,43 +1,13 @@
 package com.example
 
-import org.springframework.http.HttpStatus
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
-import javax.validation.constraints.NotEmpty
+import java.time.LocalDateTime
 
 
 @RestController
-class ApiController(private val userMapper: UserMapper) {
+class ApiController {
 
-    @GetMapping("/users")
-    fun findAll(): List<User> = userMapper.findAll()
-
-    @GetMapping("/users/{id}")
-    fun findById(@PathVariable id: Long): User =
-            userMapper.findById(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
-
-    @PostMapping("/users")
-    fun create(@RequestBody @Validated request: Request): Int {
-        val user = User(name = request.name)
-        return userMapper.insert(user)
-    }
-
-    @PutMapping("/users/{id}")
-    fun update(@PathVariable id: Long, @RequestBody @Validated request: Request): Int {
-        val user: User = userMapper.findById(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
-        user.name = request.name
-        return userMapper.update(user)
-    }
-
-    @DeleteMapping("/users/{id}")
-    fun delete(@PathVariable id: Long): Int = userMapper.delete(id);
-
-    data class Request(@field:NotEmpty val name: String)
+    @GetMapping("/")
+    fun hello(): Map<String, Any> = mapOf("datetime" to LocalDateTime.now())
 }
